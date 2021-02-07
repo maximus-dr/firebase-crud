13850d2abdcff74337f2408a750adb823192f687
Max Ivanov <maximus-dr@mail.ru>
2021-02-07 14:30:14 +0300

    Configured adding new spells to firestore

--- src/App.js
+++ src/App.js
@@ -4,23 +4,37 @@ import firebase from './firebase';
 
 function App() {
   const [spells, setSpells] = useState([]);
+  const [newSpellTitle, setNewSpellTitle] = useState('');
 
   useEffect(() => {
 
     const fetchData = async () => {
       const db = firebase.firestore();
       const data = await db.collection('spells').get();
-      setSpells(data.docs.map(doc => doc.data()));
+      setSpells(data.docs.map(doc => ({...doc.data(), id: doc.id})));
     }
 
     fetchData()
   });
 
+  const onCreate = () => {
+    const db = firebase.firestore();
+    db.collection('spells').add({
+      id: Math.random(),
+      title: newSpellTitle
+    });
+    setNewSpellTitle('');
+  }
+
   if (spells.length > 0) {
     return (
       <ul>
+        <input id="input" type="text" value={newSpellTitle} onChange={e => {
+          setNewSpellTitle(e.target.value);
+        }} />
+        <button onClick={onCreate}>Add</button>
         {spells.map(spell => (
-          <li key={spell.title}>
+          <li key={spell.id}>
             <SpellInput spell={spell} />
           </li>
         ))}
